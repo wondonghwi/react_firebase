812dcab51c904784342299e8bbec7f10983f57f4
wondonghwi <donghwi1289@gmail.com>
2021-09-02 09:05:59 +0900

    ADD : firebase9.0초기 세팅 진행 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-          
-        </div>
-    );
+  return <div>안녕! 프로젝트 시작!</div>;
 };
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8FuknfyzIOQPkULyexMjRcFUYYJ4_pW8',
+  authDomain: 'react-firebase-8c9d8.firebaseapp.com',
+  projectId: 'react-firebase-8c9d8',
+  storageBucket: 'react-firebase-8c9d8.appspot.com',
+  messagingSenderId: '612096016218',
+  appId: '1:612096016218:web:f58f94f6235e9886bafd63',
+};
+
+export default firebase.initializeApp(firebaseConfig);
