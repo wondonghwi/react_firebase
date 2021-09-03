743286e8488b3fc1f1180083299aef2053bf6a7d
wondonghwi <donghwi1289@gmail.com>
2021-09-04 08:44:43 +0900

    ADD : Auth 로그인폼 기본 틀 작성

--- src/router/Auth.js
+++ src/router/Auth.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = useCallback(e => {
+    const { name, value } = e.target;
+    setInputs({ [name]: value });
+  }, []);
+
+  const onSubmit = event => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={inputs.email || ''}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={inputs.password || ''}
+          onChange={handleChange}
+        />
+        <input type="submit" value="LogIn" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
