e0e870f3713407f7c11259fe76f1223b9f93626a
wondonghwi <donghwi1289@gmail.com>
2021-09-04 21:13:37 +0900

    ADD : firebase를 이용한 회원가입 구현 (현재 .env로 안돼서 해당부분 주석처리) -> 안되던 문제 해결해서 다시 .env로 연결해서 올림!

--- src/myfirebase.js
+++ src/myfirebase.js
@@ -1,24 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD8FuknfyzIOQPkULyexMjRcFUYYJ4_pW8',
-  authDomain: 'react-firebase-8c9d8.firebaseapp.com',
-  projectId: 'react-firebase-8c9d8',
-  storageBucket: 'react-firebase-8c9d8.appspot.com',
-  messagingSenderId: '612096016218',
-  appId: '1:612096016218:web:f58f94f6235e9886bafd63',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-//TODO .env파일로 지정하면 firebase에서 오류를 내서 일단 값 꺼내서 사용
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
-initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
