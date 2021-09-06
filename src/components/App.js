aa96f88da86e42b88aa6baed7c54d78a7c5ec143
wondonghwi <donghwi1289@gmail.com>
2021-09-07 02:01:54 +0900

    ADD : DB내용 삭제 , 업데이트 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,17 @@ import { authService } from 'myfirebase';
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged(user => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
       } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
-  return <>{init ? <RouterComponent isLoggedIn={!!isLoggedIn} userObj={userObj} /> : 'Initializing....'}</>;
+  return <>{init ? <RouterComponent isLoggedIn={!!userObj} userObj={userObj} /> : 'Initializing....'}</>;
 };
 
 export default App;
--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { dbService } from '../myfirebase';
 
 const Tweets = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const onDeleteClick = useCallback(async () => {
+    const ok = window.confirm('Are you sure you want delete?');
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  }, [tweetObj.id]);
+
+  const toggleEditing = useCallback(() => {
+    setEditing(prev => !prev);
+  }, []);
+
+  const onSubmit = useCallback(
+    async e => {
+      e.preventDefault();
+      await dbService.doc(`tweets/${tweetObj.id}`).update({
+        text: newTweet,
+      });
+      setEditing(prev => !prev);
+    },
+    [newTweet, tweetObj.id]
+  );
+
+  const onChange = useCallback(e => {
+    const { value } = e.target;
+    setNewTweet(value);
+  }, []);
+
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Tweet</button>
-          <button>Edit Tweet</button>
+          <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Edit your tweet" value={newTweet} required onChange={onChange} />
+            <button type="submit">Update Tweet</button>
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
         </>
       )}
     </div>
